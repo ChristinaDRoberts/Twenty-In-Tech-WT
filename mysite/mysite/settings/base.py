31afa588165aa41b956145e5b7661fdf255de3c8
Christina <Christinepeninsula@hotmail.com>
2019-04-27 09:51:00 -0400

    deploy

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
+from mysite import mysite as application
 
 # DATABASES['default'] = dj_database_url.config()
 
